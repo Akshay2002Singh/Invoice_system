372813cbaf70c0280138bda1741764dac4afb633
Akshay2002Singh <06akshay2002@gmail.com>
2022-08-30 16:10:59 +0530

    backend got data

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('invoice_data', views.get_invoice_data,name='get_data'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,24 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def get_invoice_data(request):
+    print("got")
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('number')
+        address = request.POST.get('address')
+        items = request.POST.get('items')
+        catagories = request.POST.get('catagories')
+        prices = request.POST.get('prices')
+        quantities = request.POST.get('quantities')
+
+        # temp_contact = contact(name=name,email=email,phone=phone,message=message)
+        # temp_contact.save()
+
+        return render(request,'next.html')
